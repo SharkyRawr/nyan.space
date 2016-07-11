ef43954f8967e7346360e57c4aa4f81f440e0f46
Sophie 'Luna' Schumann <me@luna.vg>
2016-07-11 04:16:36 -0400

    Added json API to peers.php

--- peers.php
+++ peers.php
@@ -4,48 +4,6 @@ require_once('peers.inc.php');
 // nyancoind rpc auth
 $auth = array(RPCUSER, RPCPASS);
 
-
-// templates
-require_once('lib/Twig/Autoloader.php');
-Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem('tpl');
-$twig = new Twig_Environment($loader, array(
-	'cache' => 'tpl_c',
-	'debug' => FALSE,
-));
-$twig->addFilter(new Twig_SimpleFilter('timeago', function ($datetime) {
-
-  $time = time() - $datetime;
-
-  $units = array (
-	31536000 => 'year',
-	2592000 => 'month',
-	604800 => 'week',
-	86400 => 'day',
-	3600 => 'hour',
-	60 => 'minute',
-	1 => 'second'
-  );
-
-  foreach ($units as $unit => $val) {
-	if ($time < $unit) continue;
-	$numberOfUnits = floor($time / $unit);
-	return /*($val == 'second')? 'a few seconds ago' : */
-		   (($numberOfUnits>1) ? $numberOfUnits : 'a')
-		   .' '.$val.(($numberOfUnits>1) ? 's' : '').' ago';
-  }
-
-})); // thanks http://stackoverflow.com/a/26311354
-
-$twig->addFilter(new Twig_SimpleFilter('fixport', function ($txt) {
-	$p = explode(':', $txt);
-	if(count($p) != 2) {
-		return $txt;
-	}
-	
-	return $p[0] . ':33701'; // all Nyancoin wallets listen on 33701 by default, this isn't the best solution for inbound connections but it'll have to do
-}));
-
 // json-rpc
 require_once('lib/Requests.php');
 Requests::register_autoloader();
@@ -101,6 +59,53 @@ function getPeers() {
 	
 	return array_merge($r, array('cached' => $cached));
 }
+
+if(isset($_GET['json'])) {
+	header('Content-Type: application/json');
+	die(json_encode(getpeers()));
+}
+
+
+// templates
+require_once('lib/Twig/Autoloader.php');
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('tpl');
+$twig = new Twig_Environment($loader, array(
+	'cache' => 'tpl_c',
+	'debug' => FALSE,
+));
+$twig->addFilter(new Twig_SimpleFilter('timeago', function ($datetime) {
+
+  $time = time() - $datetime;
+
+  $units = array (
+	31536000 => 'year',
+	2592000 => 'month',
+	604800 => 'week',
+	86400 => 'day',
+	3600 => 'hour',
+	60 => 'minute',
+	1 => 'second'
+  );
+
+  foreach ($units as $unit => $val) {
+	if ($time < $unit) continue;
+	$numberOfUnits = floor($time / $unit);
+	return /*($val == 'second')? 'a few seconds ago' : */
+		   (($numberOfUnits>1) ? $numberOfUnits : 'a')
+		   .' '.$val.(($numberOfUnits>1) ? 's' : '').' ago';
+  }
+
+})); // thanks http://stackoverflow.com/a/26311354
+
+$twig->addFilter(new Twig_SimpleFilter('fixport', function ($txt) {
+	$p = explode(':', $txt);
+	if(count($p) != 2) {
+		return $txt;
+	}
+	
+	return $p[0] . ':33701'; // all Nyancoin wallets listen on 33701 by default, this isn't the best solution for inbound connections but it'll have to do
+}));
 
 // render
 echo $twig->render('peers.html', array('peers' => getPeers(), 'debug' => json_encode(getPeers())));
